ada4253cfb39183ffc04da01fc170e0434a642ba
Kalengo Phiri <garethkphirij@gmail.com>
2020-04-12 18:28:38 +0200

    Added Camera page activity and connection with MENU PAGE. Camera funtionality to be added as soon as possible.
    ERRORS
    All syntax related to avoid redundant code.
    The app so far builds just fine

--- app/src/main/java/com/example/garethkalengophiri/SnapCode/SecondActivity.java
+++ app/src/main/java/com/example/garethkalengophiri/SnapCode/SecondActivity.java
@@ -1,13 +1,28 @@
 package com.example.garethkalengophiri.SnapCode;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class SecondActivity extends AppCompatActivity {
+
+    private ImageButton bCamera;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        bCamera = findViewById(R.id.imgbtn);
+
+        bCamera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(SecondActivity.this, cameractivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
